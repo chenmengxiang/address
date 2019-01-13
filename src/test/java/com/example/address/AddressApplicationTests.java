fc4bd461d227d00e581063d9eb7027bb9cae2679
chenmengxiang <57985428@qq.com>
2019-01-13 21:34:26 +0800

    Update AddressApplicationTests.java

--- src/test/java/com/example/address/AddressApplicationTests.java
+++ src/test/java/com/example/address/AddressApplicationTests.java
@@ -28,7 +28,7 @@ public class AddressApplicationTests {
 		System.out.println("ss");
 		
 		String aa = "11";
-		
+		String bb;
 		
 		System.out.println(addressService.addressResolution(userInfo));
 		
