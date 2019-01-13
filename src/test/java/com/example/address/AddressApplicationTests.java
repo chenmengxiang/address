3df3d17a6c6102f1a7036fcce0ab7874e54b8246
chenmengxiang <57985428@qq.com>
2019-01-13 21:45:43 +0800

    gg

--- src/test/java/com/example/address/AddressApplicationTests.java
+++ src/test/java/com/example/address/AddressApplicationTests.java
@@ -28,7 +28,8 @@ public class AddressApplicationTests {
 		System.out.println("ss");
 		
 		String aa = "11";
-		String bb;
+		String bb ="aa";
+		String cc = "22";
 		
 		System.out.println(addressService.addressResolution(userInfo));
 		
