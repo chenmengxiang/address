0aca07ebb2d2640fb8ed100518b8018e0be89e8a
chenmengxiang <57985428@qq.com>
2019-01-13 22:59:36 +0800

    Update AddressApplicationTests.java

--- src/test/java/com/example/address/AddressApplicationTests.java
+++ src/test/java/com/example/address/AddressApplicationTests.java
@@ -32,7 +32,7 @@ public class AddressApplicationTests {
 		String cc = "221";
 
 		cc = "2228111111";
-		String ww = "bcba";
+		String ww = "bcbaa";
 		
 		System.out.println(addressService.addressResolution(userInfo));
 		
