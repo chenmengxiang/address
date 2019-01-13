f76fcdef4b8bb1ea843c6bf4fee50f4f305b52c5
chenmengxiang <57985428@qq.com>
2019-01-13 22:22:50 +0800

    aaa

---
+++ src/test/java/com/example/address/AddressApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.address;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.address.entity.UserInfo;
+import com.example.address.service.AddressService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddressApplicationTests {
+	
+	@Autowired
+	private AddressService addressService;
+
+	@Test
+	public void contextLoads() {
+		
+		UserInfo userInfo = new UserInfo();
+		
+		userInfo.setHomeAddress1("上海市虹口区男11");
+		
+		System.out.println("ss");
+		
+		String aa = "11";
+		String bb ="aa";
+		String cc = "221";
+
+		cc = "2221888";
+		
+		System.out.println(addressService.addressResolution(userInfo));
+		
+		
+	}
+
+}
+
